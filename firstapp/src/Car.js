7907d57ec2fed5c80948bf81b56b4b9fe1f468e4
huy torres <huytorres10@gmail.com>
2022-03-09 16:44:13 +0700

    1008 create Car Xe

---
+++ firstapp/src/Car.js
@@ -0,0 +1,19 @@
+function Car() {
+	const web = <h1>I am a Car!</h1>;
+	return web;
+	// return (
+	// 	<div className="App">
+	// 		<header className="App-header">
+	// 			<img src={logo} className="App-logo" alt="logo" />
+	// 			<p>
+	// 				Edit <code>src/App.js</code> and save to reload.
+	// 			</p>
+	// 			<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+	// 				Learn React 123
+	// 			</a>
+	// 		</header>
+	// 	</div>
+	// );
+}
+
+export default Car;
---
+++ firstapp/src/Xe.js
@@ -0,0 +1,7 @@
+import React from "react";
+class Xe extends React.Component {
+	render() {
+		return <h2>Xin chao, toi la chiec xe!</h2>;
+	}
+}
+export default Xe;
