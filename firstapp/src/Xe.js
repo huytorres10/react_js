79f3af5817ba236042893db3de57fb21f1e2c9c6
huy torres <huytorres10@gmail.com>
2022-03-10 11:58:20 +0700

    1012 events

--- firstapp/src/Xe.js
+++ firstapp/src/Xe.js
@@ -1,41 +1,45 @@
 import React from "react";
 class Xe extends React.Component {
 	constructor(props) {
-		console.log("constructor");
 		super(props);
 		this.state = {
 			color: props.color,
 			value: props.value,
 		};
+		this.clickH2 = this.clickH2.bind(this);
 	}
 
 	static getDerivedStateFromProps(props, state) {
-		console.log("getDerivedStateFromProps");
 		return { color: props.color, value: props.value };
 	}
 	changeColor = () => {
-		console.log("changeColor");
 		this.setState({ color: "blue", value: "Day la chiec xe mau Xanh blue" });
 	};
+	clickH2() {
+		// alert("day la onClick H2");
+		console.log(this);
+	}
+	clickH21 = (a) => {
+		// alert("day la onClick H2");
+		console.log(this, a);
+	};
 	shouldComponentUpdate() {
-		console.log("shouldComponentUpdate", true);
 		return true;
 	}
 
 	render() {
-		console.log("render");
 		return (
 			<div>
-				<h2 style={{ color: this.state.color }}>{this.state.value}</h2>
+				<h2 onClick={() => this.clickH21("Huy")} style={{ color: this.state.color, cursor: "pointer" }}>
+					{this.state.value}
+				</h2>
 				<button onClick={this.changeColor}>Change color</button>
 			</div>
 		);
 	}
 
 	componentDidMount() {
-		console.log("componentDidMount");
 		setTimeout(() => {
-			console.log("sau 3s");
 			this.setState({ color: "yellow", value: "Day la chiec xe mau vang" });
 		}, 2000);
 	}
