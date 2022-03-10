5873dc7d2b0709dc8d460597adf8ab27a36dc03d
huy torres <huytorres10@gmail.com>
2022-03-10 09:43:07 +0700

    1010: use state react

--- firstapp/src/Xe.js
+++ firstapp/src/Xe.js
@@ -1,7 +1,14 @@
 import React from "react";
 class Xe extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			color: props.color,
+			value: props.value,
+		};
+	}
 	render() {
-		return <h2 style={{ color: this.props.color }}>{this.props.value}</h2>;
+		return <h2 style={{ color: this.state.color }}>{this.state.value}</h2>;
 	}
 }
 export default Xe;
